a83728014b70cf7fb2820c6ff8255b63e62375f3
Alex Russell-Saw <alexrussellsaw@gmail.com>
2018-05-02 17:25:30 +0100

    add RWMutex, make zero value useful

--- mutex.go
+++ mutex.go
@@ -15,17 +15,65 @@ type Mutex struct {
 var timeNow = time.Now
 
 func (m *Mutex) Lock() {
-	if !m.Sampler() {
+	if m.Sampler == nil || !m.Sampler() {
 		m.Mutex.Lock()
 		return
 	}
-	c := make(chan struct{})
+	c := make(chan struct{}, 1)
 	go func() {
 		start := timeNow()
 		<-c
-		m.Record(timeNow().Sub(start))
+		if m.Record != nil {
+			m.Record(timeNow().Sub(start))
+		}
 	}()
 	m.Mutex.Lock()
+	c <- struct{}{}
+	return
+}
+
+type RWMutex struct {
+	sync.Mutex
+
+	RecordRead func(time.Duration)
+	Record     func(time.Duration)
+	Sampler    func() bool
+}
+
+var timeNow = time.Now
+
+func (m *RMutex) Lock() {
+	if m.Sampler == nil || !m.Sampler() {
+		m.Mutex.Lock()
+		return
+	}
+	c := make(chan struct{}, 1)
+	go func() {
+		start := timeNow()
+		<-c
+		if m.Record != nil {
+			m.Record(timeNow().Sub(start))
+		}
+	}()
+	m.Mutex.Lock()
+	c <- struct{}{}
+	return
+}
+
+func (m *RMutex) RLock() {
+	if m.Sampler == nil || !m.Sampler() {
+		m.Mutex.RLock()
+		return
+	}
+	c := make(chan struct{}, 1)
+	go func() {
+		start := timeNow()
+		<-c
+		if m.RecordRead != nil {
+			m.RecordRead(timeNow().Sub(start))
+		}
+	}()
+	m.Mutex.RLock()
 	c <- struct{}{}
 	return
 }
