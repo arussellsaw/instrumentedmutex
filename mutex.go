63a282e77cf47fb9afb63ceefc3f73d18eba11ee
Alex Russell-Saw <alexrussellsaw@gmail.com>
2018-05-03 12:39:27 +0100

    provide a function to make random samplers

--- mutex.go
+++ mutex.go
@@ -1,10 +1,16 @@
 package instrumentedmutex
 
 import (
+	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
+var timeNow = time.Now
+
+// Mutex satisfies the same interface as sync.Mutex with the ability to record time taken
+// to acquire the lock
 type Mutex struct {
 	sync.Mutex
 
@@ -12,8 +18,7 @@ type Mutex struct {
 	Sampler func() bool
 }
 
-var timeNow = time.Now
-
+// Lock the mutex, recording the time taken if Sampler returns true
 func (m *Mutex) Lock() {
 	if m.Sampler == nil || !m.Sampler() {
 		m.Mutex.Lock()
@@ -26,6 +31,8 @@ func (m *Mutex) Lock() {
 	}
 }
 
+// RWMutex satisfies the same interface as sync.RWMutex with the ability to record time taken
+// to acquire the lock
 type RWMutex struct {
 	sync.RWMutex
 
@@ -34,6 +41,7 @@ type RWMutex struct {
 	Sampler    func() bool
 }
 
+// Lock the mutex, recording the time taken if Sampler returns true
 func (m *RWMutex) Lock() {
 	if m.Sampler == nil || !m.Sampler() {
 		m.RWMutex.Lock()
@@ -46,6 +54,7 @@ func (m *RWMutex) Lock() {
 	}
 }
 
+// RLock the mutex, recording the time taken if Sampler returns true
 func (m *RWMutex) RLock() {
 	if m.Sampler == nil || !m.Sampler() {
 		m.RWMutex.RLock()
@@ -55,5 +64,28 @@ func (m *RWMutex) RLock() {
 	m.RWMutex.RLock()
 	if m.RecordRead != nil {
 		m.RecordRead(timeNow().Sub(start))
+	}
+}
+
+// NewSampler returns a Sampler func that will return true `n` times in `in` calls
+func NewSampler(n, in int) func() bool {
+	s := make([]bool, in)
+	for i := range s {
+		if i < n {
+			s[i] = true
+		}
+	}
+
+	for i := range s {
+		j := rand.Intn(i + 1)
+		s[i], s[j] = s[j], s[i]
+	}
+
+	x := uint64(0)
+	return func() bool {
+		m := atomic.AddUint64(&x, uint64(1))
+		i := m % uint64(len(s))
+		v := s[i]
+		return v
 	}
 }
